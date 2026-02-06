dcbe9f2d10425f060e3704e122fa12db20f85913
Arib Zaman <aribzaman71@gmail.com>
2026-02-07 01:00:01 +0600

    trying to push frontend

--- VS_backend/index.js
+++ VS_backend/index.js
@@ -6,7 +6,7 @@ const portNum = 3001;
 app.use(cors()); 
 app.use(express.json()); // parse JSON body
 
-app.use("/api/homeAdmin", require("./routes/adminHome"));
+app.use("/api/election", require("./routes/adminHome"));
 
 app.listen(portNum, () => {
     console.log(`Backend running on http://localhost:${portNum}`);
--- VS_backend/routes/adminHome.js
+++ VS_backend/routes/adminHome.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const pool = require("../db");
 
-// GET all users
+// GET all elections
 router.get("/", async (req, res) => {
     try {
         const result = await pool.query("SELECT * FROM election");
@@ -14,16 +14,31 @@ router.get("/", async (req, res) => {
 
 // POST new user
 router.post("/", async (req, res) => {
-    const { name, email } = req.body;
+    const { name, description, start_date, end_date, status } = req.body;
 
     try {
+        
         await pool.query(
-            "INSERT INTO users (name, email) VALUES ($1, $2)",
-            [name, email]   // prevents SQL injection
+            "INSERT INTO election (name, start_date, end_date, status) VALUES ($1, $2, $3, $4)",
+            [name, start_date, end_date, status]   // prevents SQL injection
         );
-        res.status(201).json({ message: "User added" });
+        
+        res.status(201).json({ message: `election added ${name} ${description} ${start_date} ${end_date} ${status}`});
     } catch (err) {
         res.status(500).json({ error: err.message });
+    }
+});
+
+router.get("/:id", async (req, res) => {
+    const {id} = req.params;
+    try {
+        const result = await pool.query(
+            "SELECT * FROM election WHERE election_id=$1",
+            [id]
+        )
+        res.json(result.rows[0]);
+    } catch (err) {
+        res.status(500).json({error: err.message})
     }
 });
 
